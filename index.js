f5eb99f6bd883e17b36b9d784e62871918e26b63
João Victor da Silva <jv147002@gmail.com>
2023-04-20 16:57:30 -0300

    criei isso numa no javascript

--- index.js
+++ index.js
@@ -2,37 +2,106 @@
 
 import { produtos } from "./produtos.js"
 
-const criarCard = ( produto ) => {
-    const card = document.createElement('div')
-    card.classList.add('card')
+class card extends HTMLElement{
+    constructor(){
+        super()
+        this.shadow = this.attachShadow({mode:'open'})
+        this.foto = null
+        this.nome = 'Titulo do card'
+        this.description = 'descrição do produto'
+        this.preco = 'preCo'
+    }
 
-    const foto = document.createElement('img')
-    foto.classList.add('card__image')
-    foto.src = `./img/${produto.image}`
+    static get observedAttributes(){
+        return[ 'foto' ,'nome','description','preco']
+    }
 
-    const titulo = document.createElement('h5')
-    titulo.classList.add('card__title')
-    titulo.textContent = produto.name
+    attributeChangedCallback(nameAttr, oldValue, newValue){
+        this[nameAttr] = newValue
+    }
 
-    const descricao = document.createElement('p')
-    descricao.classList.add('card__description')
-    descricao.textContent = produto.description
+    connectedCallback(){
+        this.shadow.appendChild(this.component())
+        this.shadow.appendChild(this.styles())
+    }
 
-    const preco = document.createElement('spam')
-    preco.classList.add('card__price')
-    preco.textContent = `R$  ${produto.price}`
+    styles(){
+        const css = document.createElement('style')
+        css.textContent = 
+        `
+        .container{
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            gap: 16px;
+            padding: 16px;
+        }
+        
+        .card{
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+            gap: 12px;
+            border: 1px solid #ddd;
+            border-radius: 5px;
+            box-shadow: 0px 0px 8px #0006;
+            padding: 16px;
+            cursor: pointer;
+        }
+        
+        .card:hover{
+            box-shadow:0px 0px 0px #00f6 ;
+        }
+        
+        .card__image{
+            width: 200px;
+            height: 200px;
+            object-fit: contain;
+        }
+        
+        .card__title{
+            font-size: 1.5rem;
+        }
+        .card__description{
+            font-size: 1rem;
+        }
+        .card__price{
+            width: 100%;
+            font-size:  1.5rem;
+            font-weight: 900;
+        }
+        `
+        return css
+    }
 
-    card.append(foto, titulo, descricao, preco)
 
-    return card
-
+    component() {
+    
+        const card = document.createElement('div')
+        card.classList.add('card')
+    
+        const foto = document.createElement('img')
+        foto.classList.add('card__image')
+        foto.src = `${this.foto}`
+    
+        const titulo = document.createElement('h5')
+        titulo.classList.add('card__title')
+        titulo.textContent = this.nome
+    
+        const descricao = document.createElement('p')
+        descricao.classList.add('card__description')
+        descricao.textContent = this.description
+    
+        const preco = document.createElement('spam')
+        preco.classList.add('card__price')
+        preco.textContent = `R$ ${this.preco}`
+    
+        card.append(foto, titulo, descricao, preco)
+    
+        return card
+    } 
 }
 
-const carregarProdutos = () => {
-    const container = document.getElementById('container')
-    const cards =produtos.map( criarCard )
+customElements.define('card-victor', card)
 
-    container.replaceChildren(...cards)
-}
-
-carregarProdutos()
+ 
